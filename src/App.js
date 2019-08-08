b35cdb190e420d9c8720e6cfa0754a2ffed1dde2
Umar Saeed <umar.saeed@student.oulu.fi>
2019-08-09 00:23:40 +0300

    redux boilerplate done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Post';
 import Postform from './components/Postform';
 
+import store from './store.js';
+
 function App() {
   return (
-    <div className="App">
-      <header />
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header />
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/actions/postActions.js
@@ -0,0 +1,5 @@
+import { FETCH_POST, NEW_POST } from './types';
+
+export function fetchPosts() {
+  return function(dispatch) {};
+}
